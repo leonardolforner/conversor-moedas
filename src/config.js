27495bb13de8ef35a23ed8bcbbd226ca985e3868
Nrdias <natan.nrdias@hotmail.com>
2023-04-11 16:31:31 -0300

    Working on logic

---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const API_KEY = "IpLaInURpX4pn9tM2m8lJGJ7nJandf2J";
--- src/main.js
+++ src/main.js
@@ -1,21 +1,33 @@
-const moeda = document.getElementById("moeda");
-const input = document.getElementById("valorInput");
-const btn = document.getElementById("submit");
+const button = document.getElementById("converter");
+const resultDiv = document.getElementById("result");
 
-moeda.addEventListener("change", function () {
-  const currency = moeda.value;
-  const url = `https://economia.awesomeapi.com.br/json/last/${currency}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const amount = parseFloat(document.querySelector("#amount").value);
+  const from_currency = document.querySelector("#from_currency").value;
+  const to_currency = document.querySelector("#to_currency").value;
+
+  const myHeaders = new Headers();
+  myHeaders.append("apikey", "IpLaInURpX4pn9tM2m8lJGJ7nJandf2J");
+
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+    headers: myHeaders,
+  };
+
+  fetch(
+    `https://api.apilayer.com/fixer/convert?to=${to_currency}&from=${from_currency}&amount=${amount}`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const result = data.result;
+      console.log(data);
+      resultDiv.innerHTML += `<p>${amount} ${from_currency} equivale a ${result.toFixed(
+        2
+      )} ${to_currency}</p>`;
+    })
+    .catch((error) => console.log(error));
 });
-
-input.addEventListener("change", function () {
-  const currency = moeda.value;
-  const url = `https://economia.awesomeapi.com.br/json/last/${currency}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-});
-
-var teste = 0;
