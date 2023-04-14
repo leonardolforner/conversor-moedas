5c007b22d4e5813900024d74fe8825ad94a2c4a0
jrafaelbdo <130498382+jrafaelbdo@users.noreply.github.com>
2023-04-14 15:53:02 -0300

    html css com bandeiras

---
+++ src/script.js
@@ -0,0 +1,13 @@
+const convertBtn = document.getElementById('convertBtn');
+const fromCurrencySelect = document.getElementById('fromCurrency');
+const toCurrencySelect = document.getElementById('toCurrency');
+const fromAmountInput = document.getElementById('fromAmount');
+const toAmountInput = document.getElementById('toAmount');
+
+// Adicione seus códigos de API e manipulação de eventos aqui
+
+convertBtn.addEventListener('click', () => {
+  const fromCurrency = fromCurrencySelect.value;
+  const toCurrency = toCurrencySelect.value;
+  const from
+  const fromAmount = fromAmountInput.value;
