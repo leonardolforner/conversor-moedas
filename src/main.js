ea069c3d37a76fe3a9f818b3ce8ce38ab85eccc3
Nrdias <natan.nrdias@hotmail.com>
2023-04-14 15:44:23 -0300

    adicionado conversão dinâmica

--- src/main.js
+++ src/main.js
@@ -1,33 +1,40 @@
-const button = document.getElementById("converter");
 const resultDiv = document.getElementById("result");
 
-button.addEventListener("click", (event) => {
-  event.preventDefault();
+const amount = document.getElementById("amount");
 
-  const amount = parseFloat(document.querySelector("#amount").value);
-  const from_currency = document.querySelector("#from_currency").value;
-  const to_currency = document.querySelector("#to_currency").value;
+const selection = document.querySelector("select");
 
-  const myHeaders = new Headers();
-  myHeaders.append("apikey", "IpLaInURpX4pn9tM2m8lJGJ7nJandf2J");
+const converter = () => {
+  setTimeout(() => {
+    const value = parseFloat(amount.value);
 
-  const requestOptions = {
-    method: "GET",
-    redirect: "follow",
-    headers: myHeaders,
-  };
+    const from_currency = document.querySelector("#from_currency").value;
+    const to_currency = document.querySelector("#to_currency").value;
 
-  fetch(
-    `https://api.apilayer.com/fixer/convert?to=${to_currency}&from=${from_currency}&amount=${amount}`,
-    requestOptions
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      const result = data.result;
-      console.log(data);
-      resultDiv.innerHTML += `<p>${amount} ${from_currency} equivale a ${result.toFixed(
-        2
-      )} ${to_currency}</p>`;
-    })
-    .catch((error) => console.log(error));
-});
+    const myHeaders = new Headers();
+    myHeaders.append("apikey", "qU4ATf7aCktz46CTRyhVd9f7TWtCFHGV");
+
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+      headers: myHeaders,
+    };
+
+    fetch(
+      `https://api.apilayer.com/fixer/convert?to=${to_currency}&from=${from_currency}&amount=${value}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const result = data.result;
+        console.log(data);
+        resultDiv.innerHTML += `<p>${value} ${from_currency} equivale a ${result.toFixed(
+          2
+        )} ${to_currency}</p>`;
+      })
+      .catch((error) => console.log(error));
+  }, 5);
+};
+
+amount.addEventListener("input", converter);
+selection.addEventListener("change", converter);
